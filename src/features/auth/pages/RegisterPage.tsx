1271297f8f88c43b2460873dae27e51a379eca0d
loc-cv <donbb6824@gmail.com>
2022-11-30 12:57:55 +0700

    Add style for Register Page

--- src/features/auth/pages/RegisterPage.tsx
+++ src/features/auth/pages/RegisterPage.tsx
@@ -86,76 +86,123 @@ export const RegisterPage = () => {
   }
 
   return (
-    <form
-      className="flex max-w-md flex-col gap-5"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      {errorMessage && <p>{errorMessage}</p>}
+    <main className="mx-auto flex max-w-md flex-col gap-10">
+      <h1 className="text-center text-xl font-bold text-gray-800">
+        Create new account
+      </h1>
+      <form
+        className="flex flex-col gap-3 px-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {errorMessage && (
+          <p className="rounded bg-red-300 p-2 px-4 text-base text-gray-900">
+            {errorMessage}
+          </p>
+        )}
 
-      <div className="flex flex-col">
-        <label htmlFor="firstName">First name</label>
+        <div className="flex justify-between">
+          <div className="flex max-w-[49%] flex-col">
+            <label htmlFor="firstName" className="text-gray-800">
+              First name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              {...register('firstName')}
+              disabled={isLoading || isSubmitting}
+              className={`rounded p-2 ${
+                (isLoading || isSubmitting) && 'bg-gray-100'
+              }`}
+            />
+            <p className="text-sm text-red-500">{errors.firstName?.message}</p>
+          </div>
+
+          <div className="flex max-w-[49%] flex-col">
+            <label htmlFor="lastName" className="text-gray-800">
+              Last name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              {...register('lastName')}
+              disabled={isLoading || isSubmitting}
+              className={`rounded p-2 ${
+                (isLoading || isSubmitting) && 'bg-gray-100'
+              }`}
+            />
+            <p className="text-sm text-red-500">{errors.lastName?.message}</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="text-gray-800">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            {...register('email')}
+            disabled={isLoading || isSubmitting}
+            className={`rounded p-2 ${
+              (isLoading || isSubmitting) && 'bg-gray-100'
+            }`}
+          />
+          <p className="text-sm text-red-500">{errors.email?.message}</p>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="text-gray-800">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register('password')}
+            disabled={isLoading || isSubmitting}
+            className={`rounded p-2 ${
+              (isLoading || isSubmitting) && 'bg-gray-100'
+            }`}
+          />
+          <p className="text-sm text-red-500">{errors.password?.message}</p>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="passwordConfirm" className="text-gray-800">
+            Confirm password
+          </label>
+          <input
+            type="password"
+            id="passwordConfirm"
+            {...register('passwordConfirm')}
+            disabled={isLoading || isSubmitting}
+            className={`rounded p-2 ${
+              (isLoading || isSubmitting) && 'bg-gray-100'
+            }`}
+          />
+          <p className="text-sm text-red-500">
+            {errors.passwordConfirm?.message}
+          </p>
+        </div>
+
         <input
-          type="text"
-          id="firstName"
-          {...register('firstName')}
+          type="submit"
+          value={isLoading || isSubmitting ? 'Loading...' : 'Register'}
           disabled={isLoading || isSubmitting}
+          className={`w-full rounded bg-black p-2 px-4 text-lg font-medium text-gray-100 ${
+            (isLoading || isSubmitting) && 'bg-gray-600'
+          }`}
         />
-        <p>{errors.firstName?.message}</p>
-      </div>
 
-      <div className="flex flex-col">
-        <label htmlFor="lastName">Last name</label>
-        <input
-          type="text"
-          id="lastName"
-          {...register('lastName')}
-          disabled={isLoading || isSubmitting}
-        />
-        <p>{errors.lastName?.message}</p>
-      </div>
-
-      <div className="flex flex-col">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email')}
-          disabled={isLoading || isSubmitting}
-        />
-        <p>{errors.email?.message}</p>
-      </div>
-
-      <div className="flex flex-col">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          {...register('password')}
-          disabled={isLoading || isSubmitting}
-        />
-        <p>{errors.password?.message}</p>
-      </div>
-
-      <div className="flex flex-col">
-        <label htmlFor="passwordConfirm">Confirm password</label>
-        <input
-          type="password"
-          id="passwordConfirm"
-          {...register('passwordConfirm')}
-          disabled={isLoading || isSubmitting}
-        />
-        <p>{errors.passwordConfirm?.message}</p>
-      </div>
-
-      <input
-        type="submit"
-        value={isLoading || isSubmitting ? 'Loading...' : 'Register'}
-        disabled={isLoading || isSubmitting}
-      />
-
-      <p>
-        Already registered? <Link to="/login">Log into your account</Link>
-      </p>
-    </form>
+        <p className="text-center text-gray-600">
+          Already registered?{' '}
+          <Link
+            to="/login"
+            className="font-medium underline underline-offset-4 hover:underline-offset-8"
+          >
+            Log into your account
+          </Link>
+        </p>
+      </form>
+    </main>
   );
 };
