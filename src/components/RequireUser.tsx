b60a1d4bad9067e446c2f02b8e51b35024bb1450
loc-cv <donbb6824@gmail.com>
2022-11-30 16:41:10 +0700

    Add styles for loading state in RequireUser component

--- src/components/RequireUser.tsx
+++ src/components/RequireUser.tsx
@@ -1,5 +1,6 @@
 import { usersApiSlice } from 'features/users/usersApiSlice';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Spinner } from './Spinner';
 
 export const RequireUser = ({ allowedRoles }: { allowedRoles: string[] }) => {
   const location = useLocation();
@@ -8,7 +9,11 @@ export const RequireUser = ({ allowedRoles }: { allowedRoles: string[] }) => {
     usersApiSlice.endpoints.getCurrentUser.useQueryState();
 
   if (isFetching) {
-    return <p>Loading...</p>;
+    return (
+      <div className="mt-48 flex items-center justify-center">
+        <Spinner className="h-20 w-20" />
+      </div>
+    );
   }
 
   if (currentUser) {
