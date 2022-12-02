beff9eb1617cc98c1b82a3e2c2dfcec3b0964cce
loc-cv <donbb6824@gmail.com>
2022-12-03 00:39:20 +0700

    Make PostCard more responsive

--- src/features/posts/components/PostCard.tsx
+++ src/features/posts/components/PostCard.tsx
@@ -10,33 +10,44 @@ type PostCardProps = {
 
 export const PostCard = ({ post }: PostCardProps) => {
   return (
-    <article key={post._id} className="border-t-2 border-t-gray-200 py-10">
-      <div className="mb-5 flex items-center justify-between">
+    <article
+      key={post._id}
+      className="border-t-2 border-t-gray-200 py-10 lg:flex lg:justify-between"
+    >
+      <div className="mb-5 flex items-center justify-between lg:w-52 lg:items-start">
         {/* Post updated at */}
-        <div className="text-base text-gray-600 sm:text-lg">
+        <div className="text-base text-gray-600 lg:text-lg">
           {post.updatedAt}
         </div>
 
         {/* Likes/Dislikes/Save/Edit */}
-        <div className="flex justify-end gap-1">
+        <div className="flex gap-1 lg:hidden">
           <SaveButton postId={post._id} />
           <EditButton post={post} />
         </div>
       </div>
 
-      <div>
-        {/* Post author */}
-        <div className="mb-2 flex items-center gap-1">
-          <img
-            src={post.author.profilePhoto}
-            alt="Author"
-            className="h-5 w-5 rounded-full"
-          />
-          <Link to={`/profiles/${post.author.username}`} target="_blank">
-            <p className="text-sm font-bold text-gray-800">
-              {post.author.firstName} {post.author.lastName}
-            </p>
-          </Link>
+      <div className="lg:w-[60%]">
+        <div className="mb-2 flex items-baseline justify-between lg:mb-4">
+          {/* Post author */}
+          <div className="flex items-center gap-1">
+            <img
+              src={post.author.profilePhoto}
+              alt="Author"
+              className="h-5 w-5 rounded-full"
+            />
+            <Link to={`/profiles/${post.author.username}`} target="_blank">
+              <p className="text-sm font-bold text-gray-800">
+                {post.author.firstName} {post.author.lastName}
+              </p>
+            </Link>
+          </div>
+
+          {/* Likes/Dislikes/Save/Edit */}
+          <div className="hidden lg:flex lg:gap-1">
+            <SaveButton postId={post._id} />
+            <EditButton post={post} />
+          </div>
         </div>
 
         {/* Post title */}
